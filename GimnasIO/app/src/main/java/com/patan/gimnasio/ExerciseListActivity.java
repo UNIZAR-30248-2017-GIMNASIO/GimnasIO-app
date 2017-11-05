6cbc8eabe88258cd3add48cdaa2197746543b0a4
asierhandball <asierhandballer@gmail.com>
2017-11-05 10:56:00 +0100

    minor changes

--- GimnasIO/app/src/main/java/com/patan/gimnasio/ExerciseListActivity.java
+++ GimnasIO/app/src/main/java/com/patan/gimnasio/ExerciseListActivity.java
@@ -1,14 +1,44 @@
 package com.patan.gimnasio;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // En esta actividad se mostrara una lista con todos los ejercicios, ademas de una barra de busqueda para poder filtrar
 public class ExerciseListActivity extends AppCompatActivity {
+
+    private ListView l;
+    private ExerciseDbAdapter eDA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_list);
+        eDA = new ExerciseDbAdapter(this);
+        ExerciseDbAdapter.open();
+        l = (ListView)findViewById(R.id.dbExerciseList);
+        fillData();
+    }
+
+    private void fillData() {
+        // Get all of the notes from the database and create the item list
+        Cursor mNotesCursor = eDA.fetchAllExercises();
+        startManagingCursor(mNotesCursor);
+
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[] { eDA.KEY_TITLE };
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[] { R.id.text1 };
+
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter notes =
+                new SimpleCursorAdapter(this, R.layout.notes_row, mNotesCursor, from, to);
+        mList.setAdapter(notes);
     }
 }
